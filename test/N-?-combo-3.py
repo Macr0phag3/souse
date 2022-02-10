1fffb83c947208704c726e29648c32b8018866a0
Macr0phag3 <macr0phag3@qq.com>
2022-02-10 20:25:44 +0800

    支持复杂函数

--- test/N-?-combo-3.py
+++
@@ -1,3 +0,0 @@
-from math import sin
-
-k = {sin(sin(sin(sin(sin(1))))): {(1, 2): [0, f]}}
---
+++ test/combo-3.py
@@ -0,0 +1,9 @@
+from math import sin, cos, tan
+
+k = {
+    sin(cos(tan(cos(sin(tan(1)))))): {
+        sin(cos(sin(cos(sin(1))))): cos(sin(cos(sin(cos(1)))))
+    }
+}
+
+# b'cmath\nsin\np0\ncmath\ncos\np1\ncmath\ntan\np2\n(g0\n(g1\n(g2\n(g1\n(g0\n(g2\n(I1\ntRtRtRtRtRtR(g0\n(g1\n(g0\n(g1\n(g0\n(I1\ntRtRtRtRtRg1\n(g0\n(g1\n(g0\n(g1\n(I1\ntRtRtRtRtRddp3\n.'
