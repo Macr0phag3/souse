8b7ff3940aee9c316f65d3540223d59c0a37a403
Macr0phag3 <macr0phag3@qq.com>
2022-08-18 21:30:05 +0800

    发布到 PyPi，支持 pip install

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="souse",
-    version="0.0.5",
+    version="3.2.2",
     author="Tr0y",
     author_email="macr0phag3@qq.com",
     description="A tool for converting Python source code to opcode(pickle)",
@@ -14,6 +14,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/Macr0phag3/souse",
     packages=["souse"],
+    include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3.6",
         "License :: OSI Approved :: MIT License",
